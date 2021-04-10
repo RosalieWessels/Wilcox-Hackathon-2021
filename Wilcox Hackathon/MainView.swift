faa7ec1783ef182dd9922fdd78869245faf0c1d9
sking <sally@kingcastle.org>
2021-04-09 20:05:46 -0700

    progress on main page

--- Wilcox Hackathon/MainView.swift
+++ Wilcox Hackathon/MainView.swift
@@ -9,7 +9,53 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            
+            Image("studyBackground") // photo credit: https://www.lsprints.co.uk/ekmps/shops/lsprints/images/stock-designs-polka-dots-baby-blue-white-38270-p.jpg
+            
+            VStack {
+                VStack {
+                    Text("Welcome to")
+                        .font(.system(size: 35))
+                        .foregroundColor(.white)
+                        .shadow(radius:3)
+                    Text("TBD")
+                        .font(.system(size: 50))
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.white)
+                        .shadow(radius:3)
+                }
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.white)
+                        .frame(width:300, height:100)
+                        .shadow(radius:3)
+                    
+                    VStack {
+                        Text("Your Groups:")
+                            .fontWeight(.heavy)
+                        
+                        GroupBox(groupName: "Chem", groupChat: "chatLink", groupCall: "callLink")
+                    }
+                }
+            }
+            
+        }
+    }
+}
+
+struct GroupBox: View {
+    @State var groupName: String
+    @State var groupChat: String
+    @State var groupCall: String
+    var body: some View {
+        HStack {
+            Text(groupName).fontWeight(.heavy).multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+            
+            HStack {
+            }
+        }
     }
 }
 
