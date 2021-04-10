ec891d6a44712be0733dc61f72a42fe6050c1610
RosalieWessels <rosalie.wessels@yahoo.com>
2021-04-09 18:30:50 -0700

    Initial Commit

---
+++ Wilcox Hackathon/Wilcox_HackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wilcox_HackathonApp.swift
+//  Wilcox Hackathon
+//
+//  Created by Rosalie Wessels on 4/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Wilcox_HackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
