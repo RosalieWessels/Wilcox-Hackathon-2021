50ece2f060028ec4c45ce5823646c7ef319e09d8
RosalieWessels <rosalie.wessels@yahoo.com>
2021-04-09 19:13:39 -0700

    added started files

---
+++ Wilcox Hackathon/JoinGroupView.swift
@@ -0,0 +1,20 @@
+//
+//  JoinGroupView.swift
+//  Wilcox Hackathon
+//
+//  Created by Rosalie Wessels on 4/9/21.
+//
+
+import SwiftUI
+
+struct JoinGroupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct JoinGroupView_Previews: PreviewProvider {
+    static var previews: some View {
+        JoinGroupView()
+    }
+}
---
+++ Wilcox Hackathon/SignUpScreen.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpScreen.swift
+//  Wilcox Hackathon
+//
+//  Created by Rosalie Wessels on 4/9/21.
+//
+
+import SwiftUI
+
+struct SignUpScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUpScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpScreen()
+    }
+}
